de71f7ed6a85035655d5b7a30b004e61dac57749
Squinnb <quinnbarnett@hotmail.com>
2020-09-12 23:50:23 +0900

    fixed security issue

--- CapFin copy/settings.py
+++ CapFin copy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bhan41$8e!i*fr46($r=2ede7bo%9sa+!%u8)yt!+k2uf8vg^#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
